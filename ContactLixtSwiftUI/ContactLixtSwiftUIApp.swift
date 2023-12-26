8f7530cd87eb65d86d9320c41583db2be7fdce42
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-12-26 19:37:03 +0500

    Initial Commit

---
+++ ContactLixtSwiftUI/ContactLixtSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactLixtSwiftUIApp.swift
+//  ContactLixtSwiftUI
+//
+//  Created by Лилия Андреева on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactLixtSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
